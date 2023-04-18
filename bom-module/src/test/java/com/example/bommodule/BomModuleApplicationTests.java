3fcdbe0269d4aca18cf6eb9d48d15f79a2ae5862
Robert Wójcik <robert.wojcik.programista@gmail.com>
2023-04-18 14:20:06 +0200

    added child modules

--- bom-module/src/test/java/com/example/bommodule/BomModuleApplicationTests.java
+++ bom-module/src/test/java/com/example/bommodule/BomModuleApplicationTests.java
@@ -1,12 +1,12 @@
 package com.example.bommodule;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BomModuleApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
